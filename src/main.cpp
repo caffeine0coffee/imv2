330b6636fe829761c55d447aeb25caf606a3e2e4
caffeine0coffee <dev@caffeinezero.info>
2025-10-17 10:27:08 +0900

    feat: create instance w/ vk-bootstrap

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <spdlog/spdlog.h>
 
+#include "src/render_context.hpp"
+
 int main() {
-    spdlog::info("Hello, World!");
-    return 0;
+  spdlog::info("Hello, World!");
+
+  const auto *const context = Context::Instance();
+  const auto& vk_instance = context->vk_instance();
+  spdlog::info("Context created!!!");
+
+  return 0;
 }
