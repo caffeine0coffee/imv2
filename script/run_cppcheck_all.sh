4571cf15b591edc49937608752a3a3ac2fb0dd6a
caffeine0coffee <dev@caffeinezero.info>
2025-11-16 00:45:50 +0900

    fix: restore exit code and add github token

--- script/run_cppcheck_all.sh
+++ script/run_cppcheck_all.sh
@@ -4,8 +4,6 @@
 # Note that all arguments passed to this script are ignored.
 # If you want to check a single file or integrate with an editor,
 # use run_cppcheck.sh directly.
-#
-# [!] This script returns 0 regardless of cppcheck results to avoid CI failures.
 
 script_dir=$(readlink -f "$(dirname "$0")")
 project_dir=$(readlink -f "${script_dir}/..")
@@ -17,5 +15,3 @@ project_dir=$(readlink -f "${script_dir}/..")
     -i build_develop \
     -i build_release \
 	"${project_dir}"
-
-exit 0
--- script/run_cpplint_all.sh
+++ script/run_cpplint_all.sh
@@ -4,8 +4,6 @@
 # Note that all arguments passed to this script are ignored.
 # If you want to check a single file or integrate with an editor,
 # use run_cpplint.sh directly.
-#
-# [!] This script returns 0 regardless of cppcheck results to avoid CI failures.
 
 script_dir=$(readlink -f "$(dirname "$0")")
 project_dir=$(readlink -f "${script_dir}/..")
@@ -15,5 +13,3 @@ project_dir=$(readlink -f "${script_dir}/..")
 	--exclude='vcpkg' \
 	--recursive \
 	"${project_dir}"
-
-exit 0
